dc2d780a9db46d08dd04ce46b69eb7282a8d367f
Sakthi002-D <sakthivel.d002@gmail.com>
2026-06-04 15:43:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 //your code here!
-
+const list = document.getElementById("list");
 let count = 1;
 
+function addItem() {
+	const li = document.createElement("li");
+	li.textContent =`Item ${count++}`;
+	list.appendChild(li);
+}
 
 fo(let i = 0; i < 10; i++){
 	addItem();
 }
 
-
-container.addEventListner("scroll", () =>  {
+list.addEventListner("scroll", () =>  {
 	if(
-		container.scrollTop + container.clientHeight >=
-		container.scrollHeight
+		list.scrollTop + list.clientHeight >=
+		list.scrollHeight
 	) {
 		addItem();
 		addItem();
