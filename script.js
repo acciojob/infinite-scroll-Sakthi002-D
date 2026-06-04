9579ac955358e798f11851335878a41578072a16
Sakthi002-D <sakthivel.d002@gmail.com>
2026-06-04 15:30:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here!
 
+let count = 1;
+
+
+fo(let i = 0; i < 10; i++){
+	addItem();
+}
+
+
+container.addEventListner("scroll", () =>  {
+	if(
+		container.scrollTop + container.clientHeight >=
+		container.scrollHeight
+	) {
+		addItem();
+		addItem();
+	}
+});
